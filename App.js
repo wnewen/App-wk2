d0045c849ec75c7ee2ec61e8d39f98697c279b2d
wnewen <plpl541616@gmail.com>
2022-03-03 10:10:39 +0800

    songdetail

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "./node_modules/react";
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
@@ -11,6 +11,20 @@ export default function App() {
           Albums
         </Text>
       </View>
+
+      <View style={styles.cardContainerStyle}>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image 
+            style={styles.thumbnailStyle}
+            source={require('./source/Jay-Chou.jpg')} 
+          />
+          <View style={styles.headerContentStyle}>
+            <Text>Jay Chou</Text>
+            <Text>Jay Chou</Text>
+          </View>
+        </View>
+      </View>
+      
     </SafeAreaView>
   );
 }
@@ -29,4 +43,30 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 20,
   },
+  thumbnailContainerStyle: {
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  cardContainerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "#ddd",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  },
+  
 });
